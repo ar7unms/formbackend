4b0d8c7c08dbabe8d69dbbb8ed8a04212a91c051
ar7unms <msarjun077@gmail.com>
2024-02-09 14:05:32 +0530

    entry1

--- blogroute.js
+++ blogroute.js
@@ -8,21 +8,53 @@ hashPasswordGenerator=async(password)=>{
     return bcrypt.hash(password,salt)
 }
 
+router.post("/signin",async(req,res)=>{
+    let input = req.body
+    let email =req.body.email
+    let data = await user.findOne({"email":email})
+    if(!data){
+        res.json({
+            status:"invalid user"
+        })
+    }
+    console.log(data)
+    let dbpassword =data.password
+    let inputpassword= req.body.password
+    const match= await bcrypt.compare(dbpassword,inputpassword)
+    if(!match){
+        res.json({
+            status:"Invalid password"
+        })
+    }
+    res.json({
+        status:"success"
+    })
+
+})
+
 router.post("/add",async(req,res)=>{
 let {data} = {"data": req.body}
 let password=data.password
-hashPasswordGenerator(password).then(
-    (hashedPassword)=>
-    {
-        console.log(hashedPassword)
-        data.password=hashedPassword
-        console.log(data)
-let blog=new user(data)
-let result= blog.save()
+//hashPasswordGenerator(password).then(
+  //  (hashedPassword)=>
+   // {
+    //    console.log(hashedPassword)
+    //    data.password=hashedPassword
+    //    console.log(data)
+// let blog=new user(data)
+// let result= blog.save()
+// res.json({
+// status:"success"    
+// })
+// })
+// })
+const hashedPassword=await hashPasswordGenerator(password)
+data.password=hashedPassword
+let blog = new user(data)
+let result= await blog.save()
 res.json({
-status:"success"    
-})
-})
-})
+     status:"success"    
+     })
+    })
 
 module.exports=router
