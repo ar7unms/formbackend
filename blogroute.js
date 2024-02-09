7e737f84d08719378fe45bafdbfb3bf8c24fe068
ar7unms <msarjun077@gmail.com>
2024-02-09 11:45:41 +0530

    entry1

--- blogroute.js
+++ blogroute.js
@@ -1,17 +1,28 @@
 const express=require("express")
 const router=express.Router()
 const user=require("./blogmodel")
+const bcrypt =require("bcryptjs")
 
+hashPasswordGenerator=async(password)=>{
+    const salt =await bcrypt.genSalt(10)
+    return bcrypt.hash(password,salt)
+}
 
 router.post("/add",async(req,res)=>{
-let data=req.body
+let {data} = {"data": req.body}
+let password=data.password
+hashPasswordGenerator(password).then(
+    (hashedPassword)=>
+    {
+        console.log(hashedPassword)
+        data.password=hashedPassword
+        console.log(data)
 let blog=new user(data)
-let result=await blog.save()
+let result= blog.save()
 res.json({
 status:"success"    
-}
-)
-}
-)
+})
+})
+})
 
 module.exports=router
